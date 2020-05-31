3d4929555b70d519438b31c2187e4a04615f91bd
mas682 <mas682@pitt.edu>
2020-05-31 19:12:37 -0400

    Created a basic layout that can be altered as needed down the road

--- reactmovieapp/src/App.js
+++ reactmovieapp/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import background from './images/background3.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className = "App-Header">
+            <h1 class="mainTitle"> Movie App </h1>
+        </header>
+        <main>
+            <div className="mainBody">
+            </div>
+        </main>
     </div>
   );
 }
