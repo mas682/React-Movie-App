831b6734fe47f9e05c69fbcd9c056df9f72cac25
mas682 <mas682@pitt.edu>
2020-06-06 18:19:37 -0400

    added error handling to the sign up pop up..js funtionality done

--- reactmovieapp/src/SignUp.js
+++ reactmovieapp/src/SignUp.js
@@ -5,77 +5,285 @@ import Popup from 'reactjs-popup';
 import './css/signup.css';
 
 // documentation for PopUp https://react-popup.elazizi.com/component-api/
-class ControlledPopup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { open: false };
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
-  openModal() {
-    this.setState({ open: true });
-  }
-  closeModal() {
-    this.setState({ open: false });
-  }
+class SignUpPopup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            firstNameError: "",
+            lastNameError: "",
+            emailError: "",
+            passwordError: ""
+        };
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.changeHandler = this.changeHandler.bind(this);
+        this.validateForm = this.validateForm.bind(this);
+    }
 
-  render() {
+    openModal() {
+        this.setState({ open: true });
+    }
+
+    closeModal() {
+        this.setState({
+            open: false,
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            firstNameError: "",
+            lastNameError: "",
+            emailError: "",
+            passwordError: ""
+        });
+    }
+
+    validateForm(event) {
+        event.preventDefault();
+        // checks to see if email in format string@string.string
+        let validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.state.email);
+        // if firstName is empty
+        if(!this.state.firstName)
+        {
+            this.setState({"firstNameError": "First name is required"});
+        }
+        else
+        {
+            this.setState({"firstNameError": ""});
+        }
+
+        // if lastName is empty
+        if(!this.state.lastName)
+        {
+            this.setState({"lastNameError": "Last name is required"});
+        }
+        else
+        {
+            this.setState({"lastNameError": ""});
+        }
+
+        // if lastName is empty
+        if(!this.state.email)
+        {
+            this.setState({"emailError": "Email is required"});
+        }
+        else if(!this.state.email.includes("@") | !validEmail)
+        {
+            this.setState({"emailError": "You must enter a valid email address"});
+        }
+        else
+        {
+            this.setState({"emailError": ""});
+        }
+
+        // if password lenght < 8
+        if(this.state.password.length < 8)
+        {
+            this.setState({"passwordError": "Your password must be at least 8 characters"});
+        }
+        else
+        {
+            this.setState({"passwordError": ""})
+        }
+
+    }
+
+    changeHandler(event) {
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({[name]: value});
+    }
+
+    render() {
+        let firstNameInput = (
+            <React.Fragment>
+                <label>
+                    <h4 id="validLabel">First name</h4>
+                </label>
+                <input
+                    type="text"
+                    name="firstName"
+                    form = "form1"
+                    id = "validInputBox"
+                    className="inputFieldBoxShort"
+                    onChange={this.changeHandler}
+                />
+            </React.Fragment>);
+        let lastNameInput = (
+            <React.Fragment>
+                <label>
+                    <h4 id="validLabel">Last name</h4>
+                </label>
+                <input
+                    type="text"
+                    name="lastName"
+                    form = "form1"
+                    id="validInputBox"
+                    className="inputFieldBoxShort"
+                    onChange={this.changeHandler}
+                />
+            </React.Fragment>);
+
+        let emailInput = (
+            <React.Fragment>
+                <label>
+                    <h4 id="validLabel">Email</h4>
+                </label>
+                <input
+                    type="text"
+                    name="email"
+                    form = "form1"
+                    id="validInputBox"
+                    className="inputFieldBoxLong"
+                    onChange={this.changeHandler}
+                />
+            </React.Fragment>);
+
+        let passwordInput = (
+            <React.Fragment>
+                <label>
+                    <h4 id = "validLabel">Password</h4>
+                </label>
+                <input
+                    type="password"
+                    name="password"
+                    form = "form1"
+                    id = "validInputBox"
+                    className="inputFieldBoxLong"
+                    onChange={this.changeHandler}
+                />
+            </React.Fragment>);
+
+        if(this.state.firstNameError)
+        {
+            firstNameInput = (
+            <React.Fragment>
+                <label>
+                    <h4 id="errorLabel">First name</h4>
+                </label>
+                <input
+                    type="text"
+                    name="firstName"
+                    form = "form1"
+                    id = "inputBoxError"
+                    className="inputFieldBoxShort"
+                    onChange={this.changeHandler}
+                />
+                <small>{this.state.firstNameError}</small>
+            </React.Fragment>);
+        }
+
+        if(this.state.lastNameError)
+        {
+            lastNameInput = (
+                <React.Fragment>
+                    <label>
+                        <h4 id="errorLabel">Last name</h4>
+                    </label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        form = "form1"
+                        id="inputBoxError"
+                        className="inputFieldBoxShort"
+                        onChange={this.changeHandler}
+                    />
+                    <small>{this.state.lastNameError}</small>
+                </React.Fragment>);
+        }
+
+        if(this.state.emailError)
+        {
+            emailInput = (
+                <React.Fragment>
+                    <label>
+                        <h4 id="errorLabel">Email</h4>
+                    </label>
+                    <input
+                        type="text"
+                        name="email"
+                        form = "form1"
+                        id="inputBoxError"
+                        className="inputFieldBoxLong"
+                        onChange={this.changeHandler}
+                    />
+                    <small>{this.state.emailError}</small>
+                </React.Fragment>);
+        }
+
+        if(this.state.passwordError)
+        {
+            passwordInput = (
+                <React.Fragment>
+                    <label>
+                        <h4 id = "errorLabel">Password</h4>
+                    </label>
+                    <input
+                        type="password"
+                        name="password"
+                        form = "form1"
+                        id = "inputBoxError"
+                        className="inputFieldBoxLong"
+                        onChange={this.changeHandler}
+                    />
+                    <small>{this.state.passwordError}</small>
+                </React.Fragment>);
+        }
+
     return (
-      <div>
-        <button className="button" onClick={this.openModal}>
-            Sign Up Popup
-        </button>
-        <Popup
-            open={this.state.open}
-            closeOnDocumentClick
-            onClose={this.closeModal}
-        >
-            <div className="modal">
-                {/* &times is the multiplication symbol (x) --> */}
-                <a className="close" onClick={this.closeModal}>
-                &times;
-                </a>
-                <div className="header">
-                    <h3> Sign Up! </h3>
-                </div>
-                <div className="content">
-                    <form id="form1" method="post" target="/signup"/>
-                    <div className="nameContainer">
-                        <label>
-                            <h4 className="nameFieldHeader">First name</h4>
-                        </label>
-                        <input type="text" name="firstName" form = "form1" className="inputFieldBoxShort"/>
+        <div>
+            <button className="button" onClick={this.openModal}>
+                Sign Up Popup
+            </button>
+            <Popup
+                open={this.state.open}
+                closeOnDocumentClick
+                onClose={this.closeModal}
+            >
+                <div className="modal">
+                    {/* &times is the multiplication symbol (x) --> */}
+                    <a className="close" onClick={this.closeModal}>
+                    &times;
+                    </a>
+                    <div className="header">
+                        <h3> Sign Up! </h3>
                     </div>
-                    <div className="nameContainer">
-                        <label>
-                            <h4>Last name</h4>
-                        </label>
-                        <input type="text" name="lastName" form = "form1" className="inputFieldBoxShort"/>
+                    <div className="content">
+                        {/* This will eventually be a post form */}
+                        <form id="form1" onSubmit={this.validateForm} noValidate/>
+                        <div className="nameContainer">
+                            {firstNameInput}
+                        </div>
+                        <div className="nameContainer">
+                            {lastNameInput}
+                        </div>
+                        <div className="inputFieldContainer">
+                            {emailInput}
+                        </div>
+                        <div className="inputFieldContainer">
+                            {passwordInput}
+                        </div>
                     </div>
-                    <div className="inputFieldContainer">
-                        <label>
-                            <h4>Email</h4>
-                        </label>
-                        <input type="text" name="email" form = "form1" className="inputFieldBoxLong"/>
+                    <div className="actions">
+                        <button
+                            form="form1"
+                            value="create_account"
+                            className="submitButton"
+                        >CREATE YOUR ACCOUNT</button>
                     </div>
-                    <div className="inputFieldContainer">
-                        <label>
-                            <h4>Password</h4>
-                        </label>
-                        <input type="password" name="password" form = "form1" className="inputFieldBoxLong"/>
+                    <div className="accountExistsText">
+                        Already have an account? <a href="">Log In Here</a>
                     </div>
                 </div>
-                <div className="actions">
-                    <button form="form1" value="create_account" className="submitButton">CREATE YOUR ACCOUNT</button>
-                </div>
-                <div className="accountExistsText">
-                    Already have an account? <a href="">Log In Here</a>
-                </div>
-            </div>
-        </Popup>
-      </div>
-    );
-  }
+            </Popup>
+          </div>
+        );
+    }
 }
 
 const SignUp = () => {
@@ -88,7 +296,7 @@ const SignUp = () => {
                 <p> This will be changed to some other page..just a placeholder for now</p>
             </div>
             <Link to="/"><button className ="testButton">Home Page</button></Link>
-            <ControlledPopup />
+            <SignUpPopup />
         </div>
     );
 };
