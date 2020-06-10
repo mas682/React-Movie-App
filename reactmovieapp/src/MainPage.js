7d350f7814d00fbaed8a78c7260e52a50bfae475
Shawn Talbert <shawntalbs94@gmail.com>
2020-06-09 23:48:01 -0500

    just added a card to get familiar with how the posts would look. still needs work

--- reactmovieapp/src/MainPage.js
+++ reactmovieapp/src/MainPage.js
@@ -4,14 +4,19 @@ import {Link } from 'react-router-dom';
 import SignUpPopup from './SignUp.js';
 import ReviewForm from './ReviewForm.js'
 
+
 const MainPage = () => {
     return (
         <div className="mainBodyContainer">
-            <div className="mainBodyHeader">
-                <h2>Main Page</h2>
+            <div className="post">
+                <p>_theonenonly reviewed:</p>
+				<hr className="divider"></hr>
+				<img src={require("./images/The-Other-Guys-Poster.jpg")}/>
+				<p className="theGood">The Good</p>
+				<p className="theBad">The Bad</p>
             </div>
             <div className="mainBodyChild">
-                <Link to="/movie"><button className ="testButton">Movie page</button></Link>
+                <Link to="/movie"><button className="testButton">Movie page</button></Link>
                 <SignUpPopup />
                 <ReviewForm />
             </div>
--- reactmovieapp/src/MovieReview.js
+++ reactmovieapp/src/MovieReview.js
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom';
 import SignUpPopup from './SignUp.js';
 import './css/moviereview.css';
 
+
 const Movie = () => {
     return (
-        <div className="mainBodyContainer">
-            <div className="mainBodyHeader">
-                <h2>Movie Page</h2>
-            </div>
-            <div>
-                <p> This will show reviews for a specific movie</p>
-                <Link to="/"><button className ="testButton">Home Page</button></Link>
-                <SignUpPopup />
-            </div>
-        </div>
+		<div className="mainBodyContainer">
+			<div className="mainBodyHeader">
+				<h2>Movie Page</h2>
+			</div>
+			<div>
+				<p> This will show reviews for a specific movie</p>
+				<Link to="/"><button className ="testButton">Home Page</button></Link>
+				<SignUpPopup />
+			</div>
+		</div>
     );
 };
 
