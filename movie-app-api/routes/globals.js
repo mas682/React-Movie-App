eb96343b19650efaae88f604e9ea3439ce434756
mas682 <mas682@pitt.edu>
2020-06-30 22:12:45 -0400

    server side login almost done

--- movie-app-api/routes/globals.js
+++ movie-app-api/routes/globals.js
@@ -3,8 +3,41 @@
 
 
 var express = require('express');
+import models, { sequelize } from '../src/models';
 var cookieParser = require('cookie-parser');
 var router = express.Router();
 // used to sign the cookie
 router.use(cookieParser('somesecrettosigncookie'));
-export {router};
+
+
+// function to be used to verify that the cookie is valid
+// currently just checks to see if the user exists
+// may also want to add time to cookie value? but may be unneccessary
+const verifyLogin= async (cookie)=>
+{
+    let valid = false;
+    if(cookie != undefined)
+    {
+        // should be in try catch
+        // get the values from the cookie
+        let values = JSON.parse(cookie.values);
+        // for testing
+        console.log("JSON: " + values.name);
+        // see if the user is valid
+        await models.User.findByLogin('admin')
+        .then((user)=>{
+            if(user != null)
+            {
+                valid = true;
+                console.log("Cookie valid");
+            }
+            else
+            {
+                console.log("Cookie invalid");
+            }
+        });
+    }
+    return valid;
+};
+
+export {router, verifyLogin};
--- movie-app-api/routes/login.js
+++ movie-app-api/routes/login.js
@@ -1,36 +1,69 @@
 import models, { sequelize } from '../src/models';
-import {router} from './globals.js';
+import {router, verifyLogin} from './globals.js';
 const Op = require('Sequelize').Op;
 
+
+// this will need to be changed to post
 router.get("/", function(req, res, next){
-    // if the username or email are not found, create the user
-    // otherwise, do not create the user
-    console.log("COOKIE: " + req.signedCookies.test);
-    // get the signed cookies in the request
-    let cookie = req.signedCookies.test;
-    // if cookie is defined, for testing
+    // get the signed cookies in the request if there are any
+    let cookie = req.signedCookies.values;
+    // variable to indicate if user logged in
+    let valid = false;
+    // if there is a signed cookie in the request
     if(cookie != undefined)
     {
-        let undone = JSON.parse(req.signedCookies.test);
-        console.log("JSON: " + undone.name);
+        // see if the cookie has a valid user
+        verifyLogin(req.signedCookies).then((cookieValid) =>
+        {
+            if(cookieValid)
+            {
+                // set the variable to indicate cookie valid
+                valid = true;
+                res.send("You are already logged in");
+            }
+            // cookie not valid
+            else
+            {
+                checkLogin(req, res);
+            }
+        });
     }
+    // if no cookie was found
+    else
+    {
+        checkLogin(req, res);
+        // should redirect to home page at this point if login worked
+        // or let client side handle the reroute?
+    }
+});
+
+const checkLogin = (req, res) =>
+{
+    // check login and generate cookie if login allowed
     // for testing
     let pass = "password";
     // find a user by their login
+    // admin will be replaced with req.body.user
     models.User.findByLogin('admin')
     .then((user)=>{
+        // if the password is correct
+        // may want to do something like salting, not really secure
         if(user.password == pass)
         {
+            // create the valie to put into the cookie
             let value = JSON.stringify({name: user.username, email: user.email});
-            // create the cookie
-            res.cookie('test', value, {maxAge: 90000, signed: true});
+            // create the cookie with expiration in 1 day
+            res.cookie('values', value, {maxAge: 86400000, signed: true, httpOnly: true});
             res.send('created cookie');
         }
         else
         {
+            // may want to just say login denied
+            // also may want to keep track of failed login attemts and slow server
+            // down or lock account if too many failed attempts
             res.send('incorrect password');
         }
     });
-});
+};
 
 module.exports=router;
