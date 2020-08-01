3520db950f8d2b92b4827439a85513352d79be56
Shawn Talbert <shawntalbs94@gmail.com>
2020-07-31 22:12:14 -0500

    able to get response back from movie API in console.

--- reactmovieapp/src/MovieInfo.js
+++ reactmovieapp/src/MovieInfo.js
@@ -2,11 +2,6 @@ import React from 'react';
 import MainPage from './MainPage.js';
 
 
-const api = 'https://api.themoviedb.org/3/movie/';
-const movieNum = '111';
-const apiKey = '?api_key=9687aa5fa5dc35e0d5aa0c2a3c663fcc';
-
-
 class MovieInfo extends React.Component {
 	constructor(props) {
 		super(props)
@@ -15,11 +10,11 @@ class MovieInfo extends React.Component {
 		}
 	}
 	
-	componentDidMount() {
-		return fetch(api + movieNum + apiKey)
-			.then(response => response.json())
-			.then(data => this.setState({ movies: data.movies }))
-			.catch(error => {console.error(error)})
+	async componentDidMount() {
+		const url = 'https://api.themoviedb.org/3/movie/111?api_key=9687aa5fa5dc35e0d5aa0c2a3c663fcc';
+		const response = await fetch(url);
+		const data = await response.json();
+		console.log(data.overview);
 	}
 	
 	render() {
@@ -28,13 +23,7 @@ class MovieInfo extends React.Component {
 		console.error(movies)
 		
 		return (
-			<ul>
-				{movies.map(movie => 
-					<li key={movie.original_title}>
-						<a href={movie.overview}>{movie.title}</a>
-					</li>
-				)}
-			</ul>
+			<div>Test</div>
 		);
 	}
 }
