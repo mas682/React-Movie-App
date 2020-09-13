c7c4a0decdb2c60b2cd7a301907f6a5bcd560310
mas682 <mas682@pitt.edu>
2020-09-12 21:57:55 -0400

    backing up due to laptop crash

--- reactmovieapp/src/SearchDropDown.js
+++ reactmovieapp/src/SearchDropDown.js
@@ -7,10 +7,24 @@ class SearchDropDown extends React.Component {
         super(props);
         this.state = {
             value: "",
-            suggestions: [],
+            suggestions: ["abc", "def", "ced"],
+            focused: false
 
         }
         this.changeHandler = this.changeHandler.bind(this);
+        this.onFocusHandler = this.onFocusHandler.bind(this);
+        this.offFocusHandler = this.offFocusHandler.bind(this);
+    }
+
+
+    onFocusHandler()
+    {
+        alert("HERE");
+    }
+
+    offFocusHandler()
+    {
+        alert("Done");
     }
 
     changeHandler(event)
@@ -28,14 +42,20 @@ class SearchDropDown extends React.Component {
                 <label>
                     <h4 className={style.h4NoMargin}>Movie Title</h4>
                 </label>
-                <input
-                    type="text"
-                    name="value"
-                    form = "form2"
-                    className="inputFieldBoxLong validInputBox"
-                    onChange={this.changeHandler}
-                    value={this.state.value}
-                />
+                <div className={style.dropdown}>
+                    <input
+                        type="text"
+                        name="value"
+                        form = "form2"
+                        className="inputFieldBoxLong validInputBox"
+                        onChange={this.changeHandler}
+                        onFocusIn={this.onFocusHandler}
+                        // when the input is no longer focused
+                        onBlur={this.offFocusHandler}
+                        value={this.state.value}
+                    />
+                    <div>
+                </div>
             </React.Fragment>);
     }
 
