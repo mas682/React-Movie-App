d3286b69c9a9dbd17155a708a8d9ba1ec16bd829
Matt Stropkey <mas682@pitt.edu>
2021-04-06 23:02:05 -0400

    email connected to sendgrid, email account, create html template next

--- movie-app-api/routes/emailHandler.js
+++ movie-app-api/routes/emailHandler.js
@@ -2,69 +2,27 @@ import {verifyLogin} from './globals.js';
 import models, { sequelize } from '../src/models';
 var nodemailer = require('nodemailer');
 const config = require('../Config.json');
+const sgMail = require('@sendgrid/mail');
 
 // function to get information associated with the user who has the cookie
 const emailHandler = async (recipient, subject, text, html) => {
-    const transporter = nodemailer.createTransport({
-        //port: 465,
-        host: "smtp-mail.outlook.com",
-        secureConnection: true,
-        // this only allows you to send one email at a time
-        // when in real scenario, need to set this to 465
-        port: 587,
-        auth: {
-            user: config.email.username,
-            pass: config.email.password
-        },
-        tls: {
-            ciphers: 'SSLv3'
-        }
-    });
-
-    // to test connection:
-    // block out usually
-    let connected;
-    try {
-        connected = await transporter.verify().then(function(success){
-            return success
-        });
-    }
-    catch (err)
-    {
-        connected = false;
-    }
-    console.log("Connected to email server: " + connected);
-    let result = connected;
-    //
-    /*
-    //temporarily blocked out as met quoata for emails
-    // change the TO value to the recipient eventually
-    const message = {
-        from: config.email.username,  // sender address
-        to: config.email.username,   // list of receivers
-        subject: subject + "test",
+    sgMail.setApiKey(config.emailSender.key);
+    let msg = {
+        to: config.email.username,
+        from: config.emailSender.helpEmail,
+        subject: subject,
         text: text,
-        html: html,
+        html: html + "Go to http://wwww.movie-fanatics.com to unsubscribe"
     };
-    let result;
-    try {
-        result = await transporter.sendMail(message).then(function(info){
-            console.log("Email sent:");
-            console.log(info);
-            return true;
-        });
-    }
-    catch (err)
-    {
-        console.log("Error caught when sending email to: " + recipient);
-        let errorObject = JSON.parse(JSON.stringify(err));
-        console.log(errorObject);
-        result = false;
-        // not authenticated..
-        // if errorObject.code = "EAUTH"
-    }
-    */
-
+    let result = await sgMail.send(msg).then(() =>{
+        console.log('Email sent');
+        return true;
+    })
+    .catch((error) => {
+        console.log("Email not sent");
+        console.log(error);
+        return false;
+    });
 
     return result;
 };
