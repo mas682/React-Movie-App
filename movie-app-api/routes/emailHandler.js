d00840cb84726d2dba186876bc7a3d16eb54bc71
Matt Stropkey <mas682@pitt.edu>
2021-04-07 22:59:04 -0400

    working on email formatting

--- movie-app-api/routes/emailHandler.js
+++ movie-app-api/routes/emailHandler.js
@@ -5,14 +5,63 @@ const config = require('../Config.json');
 const sgMail = require('@sendgrid/mail');
 
 // function to get information associated with the user who has the cookie
-const emailHandler = async (recipient, subject, text, html) => {
+const emailHandler = async (recipient, subject, title, header, body, footer) => {
     sgMail.setApiKey(config.emailSender.key);
+    // for testing
+    header = (header === undefined) ? "Movie-Fanatics" : header;
+    title = (title === undefined) ? "Movie-Fanatics" : title;
+    body = (body === undefined) ? "" : body;
+    footer = (footer === undefined) ? `Visit us at <a href="movie-fanatics.com">movie-fanatics.com</a><br>Please
+                                       contact us if you have any questions at
+                                       <a href="mailto:help@movie-fanatics.com">help@movie-fanatics.com</a>` : footer;
+    // for testing:
+    subject = "Movie-Fanatics Verification Test";
+    header = "Movie-Fanatics";
+    title = "Email Header";
+    body = `<h2 style="color: #333">Welcome to Movie-Fanatics!</h2>This is a test paragraph for movie-fanatics.  This is a test paragraph for movie-fanatics.
+    This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics.
+    This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics.
+    This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics.`
+    footer =  `Visit us at <a href="movie-fanatics.com">movie-fanatics.com</a><br>Please contact us if you have any questions at <a href="mailto:help@movie-fanatics.com">help@movie-fanatics.com</a>`;
+    // above is for testing
+
+    let html = `
+        <html>
+            <head>
+            <title>`+ title + `</title>
+            </head>
+            <body style="background-color: #990000; min-height: calc(100vh - 100px); margin: 50px;">
+                <div style="background-color: #333; min-height: 100%;">
+                    <div style="text-align: center; height: 10%; display: flex; justify-Content: center; align-items: center;">
+                        <h1 style="color: white">` + header +`</h1>
+                    </div>
+                    <div style="min-height: calc(80% - 20px); color: gray; margin-left: 5%; margin-right: 5%; padding: 10px; text-align: left; background-color: #f9f9f9">
+                        <div style="height: calc(95% - 20px - 20%);">
+                        `+ body +`
+                        </div>
+                        <div style="height: 5%; text-align: center; color: black">
+                            ` + footer + `
+                        </div>
+                    </div>
+                    <div style="height: 10%">
+                    </div>
+                </div>
+            </body>
+        </html>
+    `;
+
+    // left off here
+    work on email formatting through gmail, then fix here...
+    issue with calc using % for some reason..
+    use vh? for margin, padding, sizes?
+
+    console.log(html);
     let msg = {
         to: config.email.username,
         from: config.emailSender.helpEmail,
         subject: subject,
-        text: text,
-        html: html + "Go to http://wwww.movie-fanatics.com to unsubscribe"
+        text: "test",
+        html: html
     };
     let result = await sgMail.send(msg).then(() =>{
         console.log('Email sent');
--- movie-app-api/routes/profile.js
+++ movie-app-api/routes/profile.js
@@ -98,6 +98,11 @@ const selectPath = (cookie, req, res, cookieValid) =>
             setImage(cookie, req, res);
             routeFound = true;
         }
+        else if(req.params.type === "emailtest")
+        {
+            emailTest(cookie, req, res);
+            routeFound = true;
+        }
     }
     else if(req.method === "POST")
     {
@@ -735,6 +740,55 @@ const setImage = async (cookie, req, res) =>
     res.status(200).send();
 }
 
+/*
+<div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5">
+
+  <p style="font-size:12px; line-height:20px;">
+    <a class="Unsubscribe--unsubscribeLink" href="{{{unsubscribe}}}" target="_blank" style="font-family:sans-serif;text-decoration:none;">
+      Unsubscribe
+    </a>
+    -
+    <a href="{{{unsubscribe_preferences}}}" target="_blank" class="Unsubscribe--unsubscribePreferences" style="font-family:sans-serif;text-decoration:none;">
+      Unsubscribe Preferences
+    </a>
+  </p>
+</div>
+*/
+const emailTest = async(cookie, req, res) =>
+{
+    let header = "Movie-Fanatics";
+    let title = "Email Header";
+    let body = `<h2 style="color: #333">Welcome to Movie-Fanatics!</h2>This is a test paragraph for movie-fanatics.  This is a test paragraph for movie-fanatics.
+    This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics.
+    This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics.
+    This is a test paragraph for movie-fanatics. This is a test paragraph for movie-fanatics.`
+    let footer =  `Visit us at <a href="movie-fanatics.com">movie-fanatics.com</a><br>Please contact us if you have any questions at <a href="mailto:help@movie-fanatics.com">help@movie-fanatics.com</a>`;
+    let html = `
+        <html>
+            <head>
+            <title>`+ title + `</title>
+            </head>
+            <body style="background-color: #990000; min-height: calc(100vh - 100px); margin: 50px;">
+                <div style="background-color: #333; min-height: 100%;">
+                    <div style="text-align: center; height: 10%; display: flex; justify-Content: center; align-items: center;">
+                        <h1 style="color: white">` + header +`</h1>
+                    </div>
+                    <div style="min-height: calc(80% - 20px); color: gray; margin-left: 5%; margin-right: 5%; padding: 10px; text-align: left; background-color: #f9f9f9">
+                        <div style="height: calc(95% - 20px - 20%);">
+                        `+ body +`
+                        </div>
+                        <div style="height: 5%; text-align: center; color: black">
+                            ` + footer + `
+                        </div>
+                    </div>
+                    <div style="height: 10%">
+                    </div>
+                </div>
+            </body>
+        </html>
+    `;
+    res.status(200).send(html);
+}
 
 
 export {profileHandler};
