7961402f66ab1708a6cc5b76045ee45d77b06cda
Matt Stropkey <mas682@pitt.edu>
2021-04-12 18:05:39 -0400

    working on dragdrop component

--- reactmovieapp/src/DragDropFile.js
+++ reactmovieapp/src/DragDropFile.js
@@ -7,18 +7,31 @@ import Alert from './Alert.js';
 class DragDropFile extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            counter: 0,
+            error: "",
+            file: undefined,
+            imageData: undefined,
+            type: "image"
+        };
         this.dragDropHandler = this.dragDropHandler.bind(this);
         this.dragOverHandler = this.dragOverHandler.bind(this);
         this.dragEnterHandler = this.dragEnterHandler.bind(this);
         this.dragLeaveHandler = this.dragLeaveHandler.bind(this);
+        this.imageValidator = this.imageValidator.bind(this);
+        this.inputImageHandler = this.inputImageHandler.bind(this);
     }
 
-    dragDropHandler(e) {
+    dragEnterHandler(e) {
         e.preventDefault();
         e.stopPropagation();
-        let droppedFiles = e.originalEvent.dataTransfer.files;
-        console.log(droppedFiles);
+        let counter = this.state.counter + 1;
+        this.setState({counter: counter});
+        if(counter === 1)
+        {
+            let form = document.querySelector('.' + style.box);
+            form.classList.add(style.dragover);
+        }
     }
 
     dragOverHandler(e) {
@@ -26,39 +39,128 @@ class DragDropFile extends React.Component {
         e.stopPropagation();
     }
 
-    dragEnterHandler(e) {
+    dragDropHandler(e, type) {
         e.preventDefault();
         e.stopPropagation();
-        let form = document.querySelector('.' + style.box);
-        form.classList.add(style.dragover);
-        console.log(form);
+        if(type === "image")
+        {
+            this.inputImageHandler(e, "drop");
+        }
     }
 
     dragLeaveHandler(e) {
         e.preventDefault();
         e.stopPropagation();
-        console.log("Leave");
-        let form = document.querySelector('.' + style.box);
-        form.classList.remove(style.dragover);
-        console.log(form);
+        let counter = this.state.counter - 1;
+        this.setState({counter: counter});
+        if(counter === 0)
+        {
+            let form = document.querySelector('.' + style.box);
+            form.classList.remove(style.dragover);
+        }
+    }
+
+    // function to validate a file is in fact a image and meets the max size requirement
+    imageValidator(files)
+    {
+        let error = "";
+        if(files.length > 1)
+        {
+            error = "Only 1 file can be selected";
+        }
+        else if(files.length < 1)
+        {
+            error = "You must select a file";
+        }
+        else
+        {
+            let file = files[0];
+            let type = file.type.toLowerCase();
+            if(!type.startsWith("image/"))
+            {
+                error = "Please select a valid image file";
+            }
+            else
+            {
+                let subtype = type.split('/')[1];
+                if(subtype !== 'jpeg' && subtype !== 'jpg' && subtype !== 'png' && subtype !== 'bmp' && subtype !== 'gif')
+                {
+                    error = "Please select a valid image file"
+                }
+                else if(file.size > 1024000)
+                {
+                    error = "Max image size is 1MB";
+                }
+                else
+                {
+                    return {
+                        image: file,
+                        error: error
+                    };
+                }
+            }
+        }
+        return {
+            image: undefined,
+            error: error
+        };
+    }
+
+    // function to handle a new file being selected by the user
+    // whether it is a drag/drop file or selected using input
+    inputImageHandler(e, type)
+    {
+        let files = (type === "drop") ? e.dataTransfer.files : e.target.files;
+        let result = this.imageValidator(files);
+        if(result.error === "")
+        {
+            let reader = new FileReader();
+            reader.readAsDataURL(result.image);
+            reader.onloadend = () => {
+                this.setState({imageData: reader.result});
+            }
+            this.setState({
+                file: result.file
+            });
+        }
+        else
+        {
+            this.setState({
+                error: result.error
+            });
+        }
     }
 
     render() {
+        let img = "";
+        if(this.state.imageData !== undefined)
+        {
+            img = <img src={this.state.imageData} />;
+        }
         return (
             <form
                 className={style.box}
                 method="post"
                 action=""
                 enctype="multipart/form-data"
-                onDrop={this.dragDropHandler}
+                onDrop={(e) => {this.dragDropHandler(e, this.state.type)}}
                 onDragOver={this.dragOverHandler}
                 onDragEnter={this.dragEnterHandler}
                 onDragLeave={this.dragLeaveHandler}
+                id={"form"}
             >
                 <div class={style.box__input}>
-                    <input class={style.box__file} type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
+                    <input
+                        class={style.box__file}
+                        type="file"
+                        accept = "image/*"
+                        onChange={(e) => {this.inputImageHandler(e, "input")}}
+                        name="files[]"
+                        id="file"
+                    />
                     <label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
                 </div>
+                {img}
                 <div class={style.box__uploading}>Uploading…</div>
                 <div class={style.box__success}>Done!</div>
                 <div class={style.box__error}>Error! <span></span>.</div>
