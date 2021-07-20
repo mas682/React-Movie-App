4129694d27df73703b78320c7e7c4ab6f74d5a7f
Matt Stropkey <mas682@pitt.edu>
2021-07-20 17:07:32 -0400

    secure set to true on cookie

--- movie-app-api/app.js
+++ movie-app-api/app.js
@@ -55,6 +55,7 @@ app.use(cors({
     origin: 'https://localhost:3000',
     credentials: true
 }));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
